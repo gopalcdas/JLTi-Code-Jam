2702cef89d4b9838ab9cfb546f11283c8ad51bb4
Gopal Das <gopalc.das@gmail.com>
2017-11-01 22:18:50 +0800

    Update TeamLunch.cpp

--- TeamLunch.cpp
+++ TeamLunch.cpp
@@ -1,3 +1,6 @@
+//JLTi Code Jam Sep 2017
+//https://gopalcdas.wordpress.com/2017/10/14/choosing-oranges/
+
 #include <stdio.h>
  
 using namespace std;
@@ -27,83 +30,84 @@ int main()
   adjMatrix[2][1] = 1;
   printf("%d\n", DaysRequired(adjMatrix, n, 1));
  
-       n = 3;
-       adjMatrix = getAdjMatrix(n);
-       //1 2
-       adjMatrix[0][1] = 1;
-       adjMatrix[1][0] = 1;
-       //2 3
-       adjMatrix[1][2] = 1;
-       adjMatrix[2][1] = 1;
-       //1 3
-       adjMatrix[0][2] = 1;
-       adjMatrix[2][0] = 1;
-       printf("%d\n", DaysRequired(adjMatrix, n, 1));
+  n = 3;
+  adjMatrix = getAdjMatrix(n);
+  //1 2
+  adjMatrix[0][1] = 1;
+  adjMatrix[1][0] = 1;
+  //2 3
+  adjMatrix[1][2] = 1;
+  adjMatrix[2][1] = 1;
+  //1 3
+  adjMatrix[0][2] = 1;
+  adjMatrix[2][0] = 1;
+  printf("%d\n", DaysRequired(adjMatrix, n, 1));
 }
  
 int **getAdjMatrix(int n)
 {
-       int **adjMatrix = new int*[n];
-       for (int i = 0; i < n; i++)
-              adjMatrix[i] = new int[n];
+  int **adjMatrix = new int*[n];
+  for (int i = 0; i < n; i++)
+    adjMatrix[i] = new int[n];
  
-       for (int i = 0; i < n; i++)
-       {
-              for (int j = 0; j < n; j++)
-                     adjMatrix[i][j] = 0;
-       }
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+      adjMatrix[i][j] = 0;
+  }
  
-       return adjMatrix;
+  return adjMatrix;
 }
  
 int DaysRequired(int **adjMatrix, int n, int m)
 {
-       for (int i = 1; i <= n; i++)
-       {
-              if (TryWithDays(adjMatrix, n, i, 1, 0, ChosenColors(n)))
-                     return i;
-       }
+  for (int i = 1; i <= n; i++)
+  {
+    if (TryWithDays(adjMatrix, n, i, 1, 0, ChosenColors(n)))
+      return i;
+  }
  
-       return 0;
+  return 0;
 }
  
 bool TryWithDays(int **adjMatrix, int n, int m, int startColor, int v, int *chosenColors)
 {
-       if (n == v)
-              return true;
+  if (n == v)
+    return true;
  
-       for (int i = startColor; i <= m; i++)
-       {
-              if (ThisColorWorks(adjMatrix, v, i, chosenColors))
-              {
-                     chosenColors[v] = i;
-                     return TryWithDays(adjMatrix, n, m, 1, v + 1, chosenColors);
-              }                   
-       }
+  for (int i = startColor; i <= m; i++)
+  {
+    if (ThisColorWorks(adjMatrix, v, i, chosenColors))
+    {
+      chosenColors[v] = i;
+      return TryWithDays(adjMatrix, n, m, 1, v + 1, chosenColors);
+    }                   
+  }
  
-       //back track
-       if (v == 0)
-              return false;
- 
-       return TryWithDays(adjMatrix, n, m, chosenColors[v-1] + 1, v - 1, chosenColors);
+  //back track
+  if (v == 0)
+    return false;
+
+  return TryWithDays(adjMatrix, n, m, chosenColors[v-1] + 1, v - 1, chosenColors);
 }
  
 bool ThisColorWorks(int **adjMatrix, int v, int color, int *chosenColors)
 {
-       for (int i = 0; i < v; i++)
-       {
-              if (adjMatrix[i][v] && chosenColors[i] == color)
-                     return false;
-       }
+  for (int i = 0; i < v; i++)
+  {
+    if (adjMatrix[i][v] && chosenColors[i] == color)
+      return false;
+  }
  
-       return true;
+  return true;
 }
- 
+
 int *ChosenColors(int n)
 {
-       int *chosenColors = new int[n];
-       for (int i = 0; i < n; i++)
-              chosenColors[i] = 0;
+  int *chosenColors = new int[n];
  
-       return chosenColors;
+  for (int i = 0; i < n; i++)
+    chosenColors[i] = 0;
+ 
+  return chosenColors;
 }
